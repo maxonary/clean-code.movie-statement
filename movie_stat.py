96c8d7d4cc24cad3a2209e4d47a58317645e51be
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-11-09 12:50:31 +0100

    Fix #1 - Extract Method

--- movie_stat.py
+++ movie_stat.py
@@ -10,49 +10,44 @@ NEW_RELEASE_PRICE_PER_DAY = 3
 CHILDREN_BASE_PRICE = 1.5
 CHILDREN_EXTRA_DAYS_COST = 1.5
 
+def calculate_regular_amount(days_rented):
+    amount = REGULAR_BASE_PRICE
+    if days_rented > 2:
+        amount += (days_rented - 2) * REGULAR_EXTRA_DAYS_COST
+    return amount
+
+def calculate_new_release_amount(days_rented):
+    return days_rented * NEW_RELEASE_PRICE_PER_DAY
+
+def calculate_children_amount(days_rented):
+    amount = CHILDREN_BASE_PRICE
+    if days_rented > 3:
+        amount += (days_rented - 3) * CHILDREN_EXTRA_DAYS_COST
+    return amount
+
+def calculate_amount(movie_price_code, days_rented):
+    if movie_price_code == REGULAR_PRICE_CODE:
+        return calculate_regular_amount(days_rented)
+    elif movie_price_code == NEW_RELEASE_PRICE_CODE:
+        return calculate_new_release_amount(days_rented)
+    elif movie_price_code == CHILDREN_PRICE_CODE:
+        return calculate_children_amount(days_rented)
+    else:
+        return 0  # Handle unknown price code
+
 def generate_rental_statement(name, rentals):
-    """
-    Generates a rental statement for a customer.
-
-    Args:
-        name (str): The name of the customer.
-        rentals (list): A list of dictionaries, where each dictionary contains information about a rental:
-            - 'movie': a dictionary with information about the movie:
-                - 'title': a string with the title of the movie.
-                - 'price_code': an integer representing the price code of the movie (0 = regular, 1 = new release, 2 = children).
-            - 'days_rented': an integer with the number of days the movie was rented for.
-
-    Returns:
-        str: A string with the rental statement, including the customer's name, the movies rented, the amount owed, and the number of frequent renter points earned.
-
-    """
-
     total_amount = 0
     frequent_renter_points = 0
     result = f"Rental Record for {name}\n"
 
     for rental in rentals:
-        this_amount = 0
-        movie = rental["movie"]
-        days_rented = rental["days_rented"]
-
-        if movie["price_code"] == REGULAR_PRICE_CODE:
-            this_amount += REGULAR_BASE_PRICE
-            if days_rented > 2:
-                this_amount += (days_rented - 2) * REGULAR_EXTRA_DAYS_COST
-        elif movie["price_code"] == NEW_RELEASE_PRICE_CODE:
-            this_amount += days_rented * NEW_RELEASE_PRICE_PER_DAY
-        else:
-            this_amount += CHILDREN_BASE_PRICE
-            if days_rented > 3:
-                this_amount += (days_rented - 3) * CHILDREN_EXTRA_DAYS_COST
-
+        this_amount = calculate_amount(rental["movie"]["price_code"], rental["days_rented"])
         frequent_renter_points += 1
 
-        if movie["price_code"] == NEW_RELEASE_PRICE_CODE and days_rented > 1:
+        if rental["movie"]["price_code"] == NEW_RELEASE_PRICE_CODE and rental["days_rented"] > 1:
             frequent_renter_points += 1
 
-        result += f"\t{movie['title']}\t{this_amount}\n"
+        result += f"\t{rental['movie']['title']}\t{this_amount}\n"
         total_amount += this_amount
 
     result += f"Amount owed is {total_amount}\n"
